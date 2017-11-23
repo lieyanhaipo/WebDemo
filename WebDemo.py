bfb6b97c8034483c15fe11e6b356a84d7e8578cd
dell <13028592007@163.com>
2017-11-23 17:31:07 +0800

    增加测试页面

--- WebDemo.py
+++ WebDemo.py
@@ -1,12 +1,20 @@
-from flask import Flask
+from flask import Flask,send_file
+from flask import abort
+from flask import redirect
 
 app = Flask(__name__)
 
-
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def index():
+    return send_file('templates/index.html')
 
+@app.route('/user/<name>')
+def sayHello(name):
+    if name == 'baidu':
+        return redirect('http://www.baidu.com')
+    elif name == 'NO':
+        return abort(404)
+    return '<h1> hello,%s </h1>' % name
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='127.0.0.1',port=8081,debug=True)
---
+++ wechat/wechatApi.py
@@ -0,0 +1,5 @@
+def wechat():
+    return 'this is a wechat test...'
+
+if __name__ == '__main__':
+    wechat()
